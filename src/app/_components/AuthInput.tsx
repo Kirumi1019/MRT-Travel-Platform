f2bdd554e7ce2ee350b923933702b7fa601e3859
Klmno1 <dennyklmno1@gmail.com>
2024-05-02 10:53:26 +0800

    Eslint & Prettier Setup

---
+++ src/app/_components/AuthInput.tsx
@@ -0,0 +1,29 @@
+import { ReactHTMLElement, useState } from "react";
+
+// Run: npx shadcn-ui@latest add input label
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+type Props = {
+  label: string;
+  type: React.HTMLInputTypeAttribute;
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+};
+
+function AuthInput({ label, type, value, setValue }: Props) {
+  return (
+    <div>
+      <Label>{label}</Label>
+      <Input
+        type={type}
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
+    </div>
+  );
+}
+
+export default AuthInput;
