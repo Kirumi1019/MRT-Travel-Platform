d76703d548fc4e344024b7cd5e362c8ee6e0194f
Klmno1 <dennyklmno1@gmail.com>
2024-05-07 11:17:45 +0800

    Create custom CredentialsProvider 2024-05-07

---
+++ src/lib/auth/index.ts
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth";
+
+import CredentialsProvider from "./CredentialsProvider";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  providers: [CredentialsProvider],
+});
