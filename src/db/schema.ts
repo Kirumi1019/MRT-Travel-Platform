47ab64f452b2a486ee72e863169402698fbbcdb8
Klmno1 <dennyklmno1@gmail.com>
2024-05-16 10:44:23 +0800

    DB schema update articles, articleLiked, articleResponse, mrtLiked

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { index, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { index, pgTable, timestamp, uuid, varchar, primaryKey, integer } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable(
   "users",
@@ -15,7 +15,71 @@ export const usersTable = pgTable(
       .default("credentials"),
   },
   (table) => ({
-    displayIdIndex: index("display_id_index").on(table.displayId),
+    displayIdIndex: index("userId_index").on(table.displayId),
     emailIndex: index("email_index").on(table.email),
   }),
 );
+
+export const articleTable = pgTable(
+  "articles",
+  {
+    displayId: uuid("display_id").defaultRandom().notNull().unique().primaryKey(),
+    authorId: uuid("author_id").notNull().references(()=>usersTable.displayId,{
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    }),
+    articleContent: varchar("article_content", {length: 500}).notNull(),
+    articleTitle: varchar("article_title", {length: 100}).notNull(),
+    mrtStation: varchar("mrt_station", {length: 20}).notNull(),
+    articleCreatedDate: timestamp("article_created_date").defaultNow().notNull(),
+  },
+  (table) => ({
+    displayIdIndex: index("articleId_index").on(table.displayId),
+  })
+)
+
+export const articleLikeTable = pgTable(
+  "article_liked",
+  {
+    articleId: uuid("article_id").notNull().references(()=>articleTable.displayId,{
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    }),
+    userId: uuid("user_id").notNull().references(()=>usersTable.displayId,{
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    }),
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.articleId, table.userId] }),  
+  })
+)
+
+export const responseTable = pgTable(
+  "articleResponse",
+  {
+    displayId: uuid("display_id").defaultRandom().notNull().unique().primaryKey(),
+    articleId: uuid("article_id").notNull().references(()=>articleTable.displayId,{
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    }),
+    userId: uuid("user_id").notNull().references(()=>usersTable.displayId,{
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    }),
+    rate: integer("article_rate").notNull(),
+    responseContent: varchar("response_content", {length: 200}),
+    responseCreatedDate: timestamp("response_created_date").defaultNow().notNull(),
+  }
+)
+
+export const mrtLikedTable = pgTable(
+  "mrt_liked",
+  {
+    userId: uuid("user_id").notNull().references(()=>usersTable.displayId,{
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    }),
+    mrtStation: varchar("mrt_station", {length: 20}).notNull(),
+  }
+)
