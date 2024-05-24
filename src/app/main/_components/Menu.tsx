e3000c395e4309c58114eab74f92648451e563d0
OSCAR WONG, WAI LOK <lokwong2001@gmail.com>
2024-05-24 16:38:18 +0800

    Update UI components for better user experience

--- src/app/main/_components/Menu.tsx
+++ src/app/main/_components/Menu.tsx
@@ -3,6 +3,7 @@ import { auth } from "@/lib/auth";
 import { publicEnv } from "@/lib/env/public";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import { Plus, User, Home, Map, Bookmark } from "lucide-react";
 
 async function Menu() {
   const session = await auth();
@@ -12,38 +13,38 @@ async function Menu() {
 
   const id = session.user.id;
   return (
-    <>
-      <div className="w-full">
-        <Link href={`/main/${id}/MyInfo`}>
-          <Button>My Info</Button>
-        </Link>
-      </div>
-      <div className="w-full">
-        <Link href={`/main/${id}/Articles`}>
-          <Button>View All Articles</Button>
-        </Link>
-      </div>
-      <div className="w-full">
-        <Link href={`/main/${id}/LikedArticles`}>
-          <Button>Saved Articles</Button>
-        </Link>
-      </div>
-      <div className="w-full">
-        <Link href={`/main/${id}/Create_Article`}>
-          <Button>Create Article</Button>
-        </Link>
-      </div>
-      <div className="w-full">
-        <Link href={`/main/MRT_Route`}>
-          <Button>MRT Route</Button>
-        </Link>
-      </div>
-      <div className="w-full">
-        <Link href={`/main/MRT_Stations`}>
-          <Button>MRT Stations</Button>
-        </Link>
-      </div>
-    </>
+    <div className="flex flex-col space-y-2 font-medium">
+      <Link href={`/main/${id}/MyInfo`}>
+        <Button variant='sidebar'>
+          <User className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-blue" /> <span className="ms-3">My Info</span>
+        </Button>
+      </Link>
+      <Link href={`/main/${id}/Articles`}>
+        <Button variant='sidebar'>
+          <Home className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-blue" /> <span className="ms-3">View Articles</span>
+        </Button>
+      </Link>
+      <Link href={`/main/${id}/LikedArticles`}>
+        <Button variant='sidebar'>
+          <Bookmark className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-blue" /> <span className="ms-3">Saved Articles</span>
+        </Button>
+      </Link>
+      <Link href={`/main/MRT_Route`}>
+        <Button variant='sidebar'>
+          <Map className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-blue" /> <span className="ms-3">MRT Route</span>
+        </Button>
+      </Link>
+      <Link href={`/main/MRT_Stations`}>
+        <Button variant='sidebar'>
+          <span className="ms-3">MRT Stations</span>
+        </Button>
+      </Link>
+      <Link href={`/main/${id}/Create_Article`}>
+        <Button variant='fab'>
+          <Plus className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-blue" />
+        </Button>
+      </Link>
+    </div>
   );
 }
 
