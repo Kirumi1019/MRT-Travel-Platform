55cee6d8bd3d13e7296da25d3025ed422fbb8687
Klmno1 <dennyklmno1@gmail.com>
2024-05-26 14:29:57 +0800

    Fixed: Next Station Problem 2024-05-26

--- src/app/api/trackInfo/route.ts
+++ src/app/api/trackInfo/route.ts
@@ -10,7 +10,7 @@ export async function GET()
     const myHeaders = new Headers();
   myHeaders.append("Content-Type", "text/xml; charset=utf-8");
 
-  const raw = "<?xml version=\"1.0\" encoding=\"utf-8\"?> \r\n<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" \r\nxmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" \r\nxmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\"> \r\n<soap:Body> \r\n<getTrackInfo xmlns=\"http://tempuri.org/\"> \r\n<userName>B10705028@ntu.edu.tw</userName> \r\n<passWord>   </passWord> \r\n</getTrackInfo> \r\n</soap:Body> \r\n</soap:Envelope>";
+  const raw = "<?xml version=\"1.0\" encoding=\"utf-8\"?> \r\n<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" \r\nxmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" \r\nxmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\"> \r\n<soap:Body> \r\n<getTrackInfo xmlns=\"http://tempuri.org/\"> \r\n<userName>B10705028@ntu.edu.tw</userName> \r\n<passWord>wh3hyS5g</passWord> \r\n</getTrackInfo> \r\n</soap:Body> \r\n</soap:Envelope>";
 
   // Remember that it will store in server cache after the first request!!!
   // So we have to overlook the data in cache ('no-store') and request again 
@@ -29,8 +29,10 @@ export async function GET()
     let result = await response.text();
     // the return data is in string type and need to be preprocess
     // in order to change the string type to JSON
+    console.log(result);
     const index = result.indexOf(']');
     result = result.substring(0,index+1);
+    // console.log(result);
     result = JSON.parse(result);
     // console.log(result[0]);
     // console.log('getting MRT api data');
